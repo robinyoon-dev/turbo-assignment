074126c4083a27ffcb136f0c7acc45d92fad608c
Robin Yoon <robinyoonofficial@gmail.com>
2025-11-10 16:44:40 +0900

    docs(web): 루트 밖 디렉토리 허용하는 설정 추가
    추후 배포 시 루트 밖 디렉토리 사용할 수 있도록 설정 추가
    
    Refs #3, #7

--- my-turborepo/apps/web/next.config.js
+++ my-turborepo/apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: ["@repo/ui"], // UI 패키지 트랜스파일링 허용
+  experimental: { externalDir: true }, // 루트 밖 디렉토리 허용
   images: {
     remotePatterns: [
       {
