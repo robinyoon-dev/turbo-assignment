72d59699ce9b1b9c664525e65a34a07305970361
Robin Yoon <robinyoonofficial@gmail.com>
2025-11-11 15:18:40 +0900

    fix(web): 이전 버튼 위치 수정

--- my-turborepo/apps/web/app/result/layout.tsx
+++ my-turborepo/apps/web/app/result/layout.tsx
@@ -7,9 +7,7 @@ export default function ResultLayout({ children }: { children: React.ReactNode }
         <div className="min-h-screen bg-gray-400 flex flex-col">
             <Header text={NAME} />
             {children}
-            <div className="px-6 pb-8 md:px-8 md:pb-10 lg:px-16 lg:pb-12">
-                <PreviousButton />
-            </div>
+
         </div>
     );
 }
